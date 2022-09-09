0fe4a75a89812def4a957efa383f013851fa7abe
SofyeneBou <sofyene.bouguerra@esprit.tn>
2022-09-09 22:15:59 +0100

    Creation Project

---
+++ src/main/java/exam/portal/tn/ExamPortralApplication.java
@@ -0,0 +1,13 @@
+package exam.portal.tn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamPortralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamPortralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/exam/portal/tn/ExamPortralApplicationTests.java
@@ -0,0 +1,13 @@
+package exam.portal.tn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamPortralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
